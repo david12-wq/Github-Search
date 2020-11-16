22fe87583f00ed819f49c3dd0141686f7246251f
david12-wq <davidkamau245@gmail.com>
2020-11-16 16:15:23 +0300

    Added repos.service.ts

--- src/app/repos.service.ts
+++ src/app/repos.service.ts
@@ -1,9 +1,21 @@
+import { HttpClient } from '@angular/common/http';
 import { Injectable } from '@angular/core';
+import { Observable } from 'rxjs';
+import { environment } from 'src/environments/environment';
+import { Repos } from './repos';
 
 @Injectable({
   providedIn: 'root'
 })
 export class ReposService {
+  repo:Repos[]=[];
 
-  constructor() { }
+  keyUrl='https://api.github.com/users/';
+token=`?access_token=${environment.key}`;
+
+  constructor(public http:HttpClient) { }
+
+  findRepo(UserName:string): Observable<any>{
+    return this.http.get(this.keyUrl+UserName+ '/repos'+this.token)
+  }
 }
