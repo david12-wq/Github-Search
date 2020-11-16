bf90a8f11e583643a333f5c4996336879d54db1a
david12-wq <davidkamau245@gmail.com>
2020-11-16 15:41:31 +0300

    modified profil.component.ts

--- src/app/profile/profile.component.ts
+++ src/app/profile/profile.component.ts
@@ -1,15 +1,36 @@
 import { Component, OnInit } from '@angular/core';
+import { Repos } from '../repos';
+import { ReposService } from '../repos.service';
+import { User } from '../user';
+import { UsersService } from '../users.service';
 
 @Component({
   selector: 'app-profile',
-  templateUrl: './profile.component.html',
+ templateUrl: './profile.component.html',
   styleUrls: ['./profile.component.css']
 })
 export class ProfileComponent implements OnInit {
-
-  constructor() { }
-
+user:User[];
+repo:Repos[];
+  constructor(public serviceUser:UsersService, public serviceRepo:ReposService) { }
+findGitUser(UserName){
+  this.serviceUser.findUser(UserName).then(
+    (success)=>{
+      this.user=this.serviceUser.user;
+    },
+    (error)=>{
+      console.log(error)
+    }
+  );
+  this.serviceRepo.findRepo(UserName).subscribe(
+    (success)=>{
+      this.repo=success;
+      return(this.repo)
+    }
+  )
+}
   ngOnInit(): void {
+    this.findGitUser('david12-wq');
   }
 
 }
